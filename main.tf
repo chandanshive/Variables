98fd9d01deaa90c94a6a8ac13ffb55c06686317b
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-07-01 23:12:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "variable_bucket" {
   for_each = toset( ["B1-20210107", "B2-20210107", "B3-20210107"] )
   bucket   = each.key
+  
+  tags = {
+    Name    = each.key
+  }
 }
